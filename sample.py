a604baf3c4b4d77730b7d76a05543518d34ac824
ercintorun <ercintorun@gmail.com>
2020-09-24 10:20:58 +0300

    Delete sample.py

--- sample.py
+++
@@ -1,8 +0,0 @@
-import simplefetch,logging
-logging.basicConfig(filename='info.log', filemode='a', level=logging.INFO,
-                    format='%(asctime)s [%(name)s] %(levelname)s (%(threadName)-10s): %(message)s')
-					
-test_router = simplefetch.SSH("192.168.1.1",22, "admin", "secret", "cisco-ios")
-print (test_router.fetchdata("show version"))
-test_router.disconnect() 
-
